abfde0956d75779efa93916f957cf1243ec18e64
JorgeRodriguez <jrr150502@gmail.com>
2022-11-10 12:16:09 +0100

    App.java y README.md modificados

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Jorge Rodríguez Rabanal");
+    System.out.println("10/11/2022");
+    System.out.println("Valdelafuente");
   }
-  
 }
